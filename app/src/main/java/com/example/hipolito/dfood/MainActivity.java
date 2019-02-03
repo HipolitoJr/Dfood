84c6625096eb5ae1ef8c7c3467c43bdf760730c8
HipolitoJr <hipolitocavalcantejr@gmail.com>
2019-02-03 15:37:24 -0300

    Update cadastre activity

--- app/src/main/java/com/example/hipolito/dfood/MainActivity.java
+++ app/src/main/java/com/example/hipolito/dfood/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.hipolito.dfood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView btnCadastreSe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupViews();
+    }
+
+    private void setupViews() {
+        btnCadastreSe = findViewById(R.id.btn_cadastre_se);
+
+        btnCadastreSe.setOnClickListener(onclick -> {
+            startActivity(new Intent(MainActivity.this, CadastreActivity.class));
+        } );
     }
 }
